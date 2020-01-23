85205f299b93eaea9f6355ec3abcdb5202931efd
Dan Doucette <danieldoucette@mcdougallenergy.com>
2020-01-23 14:40:39 -0500

    Initial commit

---
+++ StringJoiner/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StringJoiner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void JoinTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            string join = ((Button)sender).Tag.ToString();
+            var bookend = "";
+            if(join.Contains("'"))
+            {
+                bookend = "'";
+            }
+
+            DestinationTextBox.Text = $"{bookend}{string.Join(join, SourceTextBox.Text.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))}{bookend}";
+        }
+    }
+}
